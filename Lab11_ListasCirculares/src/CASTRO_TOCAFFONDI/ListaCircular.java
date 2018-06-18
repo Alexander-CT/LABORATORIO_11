dfe699d5ed2ad64e6100401514f32021d84bdea7
CASTRO TOCAFFONDI <alexanderpierocastrotocaffondi@hotmail.com>
2018-06-17 21:13:53 -0500

    Laboratorio 11 - Listas Circulares

--- Lab11_ListasCirculares/src/CASTRO_TOCAFFONDI/ListaCircular.java
+++ Lab11_ListasCirculares/src/CASTRO_TOCAFFONDI/ListaCircular.java
@@ -1,16 +1,44 @@
 package CASTRO_TOCAFFONDI;
 //Autor: CASTRO TOCAFFONDI
+//import java.util.ArrayList;
+//import javax.swing.JOptionPane;
 public class ListaCircular<T> {
-    private Nodo<T> raiz;
+    /*private ArrayList<Nodo> lista;
+    public void agregarNodo(Nodo<T> n){
+        lista.add(n);
+    }
+    public boolean esVacia(){
+        return lista==null || lista.size()==0;
+    }
+    public int cantidad(){
+        int contador=0;
+        for(Nodo obj: lista){
+            contador++;
+        }
+        return contador;
+    }
+    public void mostrar(){
+        if(!esVacia()){
+            int cont = 0;
+            System.out.println("-------------------\nNODOS ALMACENADOS:\n-------------------");
+            for(Nodo obj: lista){
+                cont++;
+                System.out.println(cont+")  "+ obj.getDato()+"\n---------------------------------------");//JOptionPane.showInputDialog("Notas"+ obj.getNota());
+            }    
+        }else{
+            JOptionPane.showMessageDialog(null,"No hay nodos en la lista","ERROR: Lista vacia",JOptionPane.ERROR_MESSAGE);
+        }
+    }*/
+    Nodo<T> raiz;
     public ListaCircular(){
         raiz=null;
     }
-    public boolean esVacia(){//*****************************
-        Nodo<T> aux=null;
-        return (raiz==aux);
+    public boolean esVacia(){
+        return (raiz==null);
     }
-    public void insertar(T dato){
-        Nodo<T> nuevo=new Nodo<>(dato);
+    public ListaCircular insertar(T dato){
+        /*Nodo<T> nuevo = new Nodo(dato);
+        nuevo.setDato(dato);
         if(!esVacia()){
             raiz=nuevo;
             raiz.setSiguiente(nuevo);//nuevo.setSiguiente(raiz);
@@ -21,7 +49,14 @@ public class ListaCircular<T> {
             }
             aux.setSiguiente(nuevo);
             nuevo.setSiguiente(raiz);
+        }*/
+        Nodo<T> nuevo=new Nodo<>(dato);
+        if(!esVacia()){
+            raiz.setSiguiente(nuevo);
+            raiz=raiz.getSiguiente();
         }
+        raiz=nuevo;
+        return this;
     }
     public int cantidad(){
         int contador=0;
@@ -37,10 +72,12 @@ public class ListaCircular<T> {
     public void imprimirDoWhile(){
         if(!esVacia()){
             Nodo<T> aux=raiz;
+            String cadena="";
             do{
-                System.out.println(aux.getDato()+"...............");
+                cadena=cadena+"[ "+aux.getDato()+" ]-->";
                 aux=aux.getSiguiente();
-            }while(aux!=raiz);
+            }while(aux!=raiz.getSiguiente());//No era siguiente, era raiz.getSiguiente, ahora funciona.
+            System.out.println(cadena);
         }
     }
     public void imprimirWhile(){
--- Lab11_ListasCirculares/src/CASTRO_TOCAFFONDI/Menu.java
+++ Lab11_ListasCirculares/src/CASTRO_TOCAFFONDI/Menu.java
@@ -4,17 +4,17 @@ import javax.swing.JOptionPane;
 public class Menu<T> {
     public Menu(){
         ListaCircular<T> Lc=new ListaCircular<>();
-        T element=null;
-        int opcion=0;
+        T element = null;
         String mensaje="1. Agregar nodo\n";
         mensaje+="2. Mostrar\n";
         mensaje+="3. Salir";
+        int opcion;
         do{
             opcion=Integer.parseInt(JOptionPane.showInputDialog(null,mensaje,"MENÚ",JOptionPane.INFORMATION_MESSAGE));
             switch(opcion){
                 case 1: //AGREGAR NODO
-                    element=(T) String.valueOf(JOptionPane.showInputDialog(null,"Ingresa el dato","Ingresar dato",JOptionPane.INFORMATION_MESSAGE));
-                    Lc.insertar((T) element);
+                    element=(T) String.valueOf(JOptionPane.showInputDialog(null,"Ingresa el elemento"));
+                    Lc.insertar(element);
                     break;
                 case 2: //MOSTRAR
                     Lc.imprimirDoWhile();
